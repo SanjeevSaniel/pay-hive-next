aefa6caa04337b745271237c8f39cad8cadf3910
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-01-30 03:29:46 +0530

    Build create group api

--- src/app/(frontend)/v1/groups/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-const GroupDetails = ({ params }: { params: { groupId: string } }) => {
-  const { groupId } = params;
-
-  return <div>GroupDetails {groupId}</div>;
-};
-
-export default GroupDetails;
---
+++ src/app/api/groups/create/route.ts
@@ -0,0 +1,43 @@
+import { connectDB } from '@/dbConfig/dbConfig';
+import Group from '@/models/groupModel';
+import { NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
+
+// Connect to MongoDB
+connectDB();
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { groupName, description, memberIds, isGroup } = body;
+
+  // Validate input
+  if (!groupName || !memberIds || !Array.isArray(memberIds)) {
+    return NextResponse.json({ error: 'Invalid input data' }, { status: 400 });
+  }
+
+  try {
+    const newGroup = new Group({
+      groupId: uuidv4(), // Generate a unique ID for the group
+      groupName,
+      description,
+      memberIds,
+      isGroup,
+    });
+
+    // Save the new group to the database
+    const savedGroup = await newGroup.save();
+
+    return NextResponse.json(savedGroup, { status: 201 });
+  } catch (error: unknown) {
+    // Safely handle the error and return a JSON response with appropriate details
+    return NextResponse.json(
+      {
+        error: 'Failed to create group',
+        details:
+          error instanceof Error ? error.message : 'An unknown error occurred',
+      },
+      { status: 500 },
+    );
+  }
+
+}
