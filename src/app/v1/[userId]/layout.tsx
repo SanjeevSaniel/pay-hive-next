ed5b4583128b1162d6e7760d609aba9e6179bdad
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-01 03:57:35 +0530

    feat: add AppPageLayout component for user-specific page structure

---
+++ src/app/v1/[userId]/layout.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Header from '@/components/Header';
+import Navbar from '@/components/Navbar';
+import useLenis from '@/hooks/useLenis';
+
+const AppPageLayout = ({ children }: { children: React.ReactNode }) => {
+  useLenis();
+
+  return (
+    <div className='grid grid-rows-layout min-h-screen'>
+      <div className='p-2'>
+        <Header />
+      </div>
+      <div className='p-2 mb-14 overflow-auto'>{children}</div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default AppPageLayout;
