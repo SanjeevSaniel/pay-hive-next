614c81a57a0252a38d02b3fa9d361094a2812675
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-01-31 20:16:39 +0530

    feat: update Header component styling with new background color and rounded corners

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Bell } from 'lucide-react';
 
 const Header = () => {
   return (
-    <div className='flex justify-between items-center px-6 py-3 bg-[#f0efe2] sticky top-0 z-20'>
+    <div className='flex justify-between items-center px-6 py-3 bg-[#f1f1f1] rounded-xl sticky top-0 z-20'>
       <span className={`${neodaFont.className} text-xl mb-1`}>S</span>
       <span className='text-lg font-extrabold'>Splitly</span>
       <Bell size={22} />
