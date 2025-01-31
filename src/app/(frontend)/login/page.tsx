6042001e586e2918a8d4f9706e274dc512130c8e
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-01 01:23:10 +0530

    fix: update import statement for LoginForm component to default import

--- src/app/(frontend)/login/page.tsx
+++ src/app/(frontend)/login/page.tsx
@@ -1,5 +1,5 @@
 import { GalleryVerticalEnd } from 'lucide-react';
-import { LoginForm } from '@/components/Authentication/login-form';
+import LoginForm from '@/components/Authentication/login-form';
 import Link from 'next/link';
 
 const LoginPage = () => {
