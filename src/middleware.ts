6a9fe7ba68334259c0217f934ad497bc0f31db2e
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-01 03:56:54 +0530

    feat: update middleware to handle user-specific redirection and adjust public path checks

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,27 +3,30 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-
-  const isPublicPath = path === '/login' || path === '/signup';
-
   const token = request.cookies.get('token')?.value || '';
 
+  // Public paths
+  const isPublicPath = path === '/v1/login' || path === '/v1/signup';
+
   if (isPublicPath && token) {
-    return NextResponse.redirect(new URL('/v1/groups', request.nextUrl));
+    const userId = 'someUserId'; // Replace this with actual logic to extract userId from the token or request
+    return NextResponse.redirect(new URL(`/v1/${userId}/`, request.nextUrl));
   }
 
   if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL('/login', request.nextUrl));
+    return NextResponse.redirect(new URL('/v1/login', request.nextUrl));
   }
+
+  // Continue to next middleware or route handler if no redirection is needed
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
     '/v1/:path*',
-    '/v1/groups',
-    '/v1/groups/:path*',
-    '/v1/profile/:path*',
-    '/login',
-    '/signup',
+    '/v1/:userId/groups',
+    '/v1/:userId/groups/:path*',
+    '/v1/login',
+    '/v1/signup',
   ],
 };
