2914f08e694dfe5a23e925c9c5cab4f5b99c8461
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-01 01:23:27 +0530

    fix: add aria-labels for accessibility in HomePage component

--- src/app/(frontend)/v1/page.tsx
+++ src/app/(frontend)/v1/page.tsx
@@ -50,45 +50,51 @@ const HomePage = () => {
           asChild
           size='icon'
           variant='ghost'
-          className='p-0'>
+          className='p-0'
+          aria-label='Add Expense'>
           <Plus size={26} />
         </Button>
       </div>
-      <div className='flex overflow-x-scroll scroll-smooth space-x-4 p-2'>
+      <div
+        className='flex overflow-x-scroll scroll-smooth space-x-4 p-2'
+        aria-label='Expenses Categories'>
         {categories.map((item) => (
           <Card
+            aria-label={`Expense Category: ${item.category}`}
             key={item.title}
             className='flex-shrink-0 w-fit text-card rounded-xl mb-4'
             style={{ backgroundColor: item.bgColor }}>
             <CardHeader
               className='py-3 rounded-t-xl'
               style={{ backgroundColor: item.headerBgColor }}>
-              <div>
-                <CardTitle className='text-xl'>{item.title}</CardTitle>
-                <CardDescription className='text-card'>
-                  {item.description}
-                </CardDescription>
-              </div>
+              <CardTitle
+                aria-label='Category Amount'
+                className='text-xl'>
+                {item.title}
+              </CardTitle>
+              <CardDescription
+                aria-label='Category Timeframe'
+                className='text-card'>
+                {item.description}
+              </CardDescription>
             </CardHeader>
             <CardContent className='flex justify-between items-center gap-4 py-2'>
-              <div>
+              <div aria-label='Category Info'>
                 <p className='text-lg'>{item.percentage}%</p>
                 <p className='text-sm'>{item.category}</p>
               </div>
-
-              <div>
-                <CircularProgress
-                  classNames={{
-                    svg: 'w-16 h-16 drop-shadow-sm',
-                    indicator: 'stroke-white',
-                    track: 'stroke-white/10',
-                    value: 'text-sm font-semibold text-white',
-                  }}
-                  showValueLabel={true}
-                  strokeWidth={4}
-                  value={item.value}
-                />
-              </div>
+              <CircularProgress
+                classNames={{
+                  svg: 'w-16 h-16 drop-shadow-sm',
+                  indicator: 'stroke-white',
+                  track: 'stroke-white/10',
+                  value: 'text-sm font-semibold text-white',
+                }}
+                showValueLabel={true}
+                strokeWidth={4}
+                value={item.value}
+                aria-label={`Progress for ${item.category}`}
+              />
             </CardContent>
           </Card>
         ))}
