a9666be15609ceee1ddb5c88bb8b7fab89358951
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-01 03:58:18 +0530

    feat: update Header component to extract userId from query parameters for dynamic basePath

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
-import { useBasePath } from '@/context/BasePathContext';
+'use client';
+
 import { neodaFont } from '@/fonts/Neoda/neodaFont';
 import { Bell } from 'lucide-react';
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
 const Header = () => {
-  const basePath = useBasePath();
+  const searchParams = useSearchParams();
+  const userId = searchParams.get('userId'); // Extract userId from the query parameters
+
+  const basePath = `/v1/${userId}`;
 
   return (
     <div className='flex justify-between items-center px-6 py-3 bg-[#424141] text-white rounded-xl sticky top-0 z-20'>
       <span className={`${neodaFont.className} text-xl mb-1`}>S</span>
       <Link
-        href={`${basePath}`}
+        href={basePath}
         className='text-lg font-extrabold'>
         Splitly
       </Link>
