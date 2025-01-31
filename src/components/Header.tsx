3c8e0286b8876791c47def7ccddc0f30a58290bb
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-01 01:25:04 +0530

    feat: update Header component to use Link for navigation and change background color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
+import { useBasePath } from '@/context/BasePathContext';
 import { neodaFont } from '@/fonts/Neoda/neodaFont';
 import { Bell } from 'lucide-react';
+import Link from 'next/link';
 
 const Header = () => {
+  const basePath = useBasePath();
+
   return (
-    <div className='flex justify-between items-center px-6 py-3 bg-[#f1f1f1] rounded-xl sticky top-0 z-20'>
+    <div className='flex justify-between items-center px-6 py-3 bg-[#424141] text-white rounded-xl sticky top-0 z-20'>
       <span className={`${neodaFont.className} text-xl mb-1`}>S</span>
-      <span className='text-lg font-extrabold'>Splitly</span>
+      <Link
+        href={`${basePath}`}
+        className='text-lg font-extrabold'>
+        Splitly
+      </Link>
       <Bell size={22} />
     </div>
   );
