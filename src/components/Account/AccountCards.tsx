54481e2469fcb10f2d363d874f916881947cf2a0
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-05 00:20:45 +0530

    refactor: update AccountCards component to use UserIcon and manage current user state

--- src/components/Account/AccountCards.tsx
+++ src/components/Account/AccountCards.tsx
@@ -14,7 +14,7 @@ import {
   Settings,
   Shield,
   Sliders,
-  User,
+  UserIcon,
 } from 'lucide-react';
 import Link from 'next/link';
 import LogoutButton from '../LogoutButton';
@@ -23,13 +23,15 @@ import useAppStore from '@/stores/useAppStore';
 import { useParams } from 'next/navigation';
 import { Button } from '@heroui/react';
 import { FaEdit } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { User } from '@/types/types';
 
 const TABS = [
   {
     key: 'personalInformation',
     label: 'Personal Information',
     description: 'Update your personal details.',
-    icon: User,
+    icon: UserIcon,
     path: '/personal-information',
   },
   {
@@ -76,43 +78,50 @@ const AccountCards = () => {
 
   // Extract userId from the URL params
   const { userId } = params;
-  console.log(userId);
 
-  // Fetch the current user based on userId
-  const user = users.find((user) => user.userId === userId);
-  console.log(user);
+  // State to store the current user
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (userId && users) {
+      const user = users.find((user) => user.userId === userId) || null;
+      setCurrentUser(user);
+    }
+  }, [userId, users]);
 
   if (!basePath) {
     return <div>Loading...</div>; // Display loading indicator until basePath is available
   }
 
   return (
-    <div className='flex flex-col space-y-4 p-2'>
-      <div className='text-center text-2xl font-extrabold px-2'>Account</div>
-      {user && (
+    <div className='flex flex-col space-y-4 p-2 text-stone-100'>
+      <div className='text-center text-2xl font-extrabold mt-4 px-2'>
+        Account
+      </div>
+      {currentUser && (
         <div className='flex flex-col gap-2 items-center'>
           <Avatar
             className='w-20 h-20'
             src={
-              user.profileImageUrl ||
+              currentUser.profileImageUrl ||
               'https://i.pravatar.cc/150?u=a04258114e29026708c'
             }
           />
           <div className='flex flex-col items-center gap-0'>
             <div className='flex items-center gap-1 flex-wrap'>
-              <span className='text-lg font-semibold'>{user.name}</span>
+              <span className='text-lg font-semibold'>{currentUser.name}</span>
               <span className='text-xs text-gray-400 font-semibold'>
-                [@{user.userId}]
+                [@{currentUser.userId}]
               </span>
             </div>
 
             <span className='text-sm text-gray-400 font-semibold'>
-              {user.email}
+              {currentUser.email}
             </span>
           </div>
           <Button
             startContent={<FaEdit size={18} />}
-            size='sm'
+            size='md'
             color='default'
             variant='flat'>
             Edit Profile
@@ -126,7 +135,7 @@ const AccountCards = () => {
             key={tab.key}
             href={`${basePath}/account${tab.path}`}
             passHref>
-            <Card className='px-2 pb-2 border border-gray-50 shadow-sm rounded-2xl'>
+            <Card className='px-2 pb-2 bg-stone-700 text-stone-100 border-stone-700 shadow-sm rounded-2xl'>
               <CardHeader className='grid grid-cols-[auto_1fr_auto] gap-2 p-2'>
                 <div className='flex justify-center items-center m-2'>
                   <tab.icon />
@@ -135,7 +144,7 @@ const AccountCards = () => {
                   <CardTitle className='flex items-center gap-2 text-lg'>
                     {tab.label}
                   </CardTitle>
-                  <CardDescription className='flex items-center space-x-2 text-md'>
+                  <CardDescription className='flex items-center space-x-2 text-md text-stone-400'>
                     {tab.description}
                   </CardDescription>
                 </div>
