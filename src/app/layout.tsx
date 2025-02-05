5cf5a5e18cd93e8dcb4ab3ac93551b0363314995
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-05 05:47:23 +0530

    feat: add HeroUIProvider to layout and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Comfortaa } from 'next/font/google';
 import './globals.css';
+import { Providers } from './providers';
 
 const comfortaa = Comfortaa({
   weight: ['400', '700'], // specify the weights you need
@@ -21,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${comfortaa.className} antialiased min-h-screen`}>
-        <div>{children}</div>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
