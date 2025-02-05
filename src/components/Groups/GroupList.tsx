db63a766f5a838f582a86a9d06c615d679766a94
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-05 12:02:10 +0530

    feat: add GroupList component for displaying and managing groups with default selection

---
+++ src/components/Groups/GroupList.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import useAppStore from '@/stores/useAppStore';
+import GroupCard from '@/components/Groups/GroupCard';
+import { useEffect, useMemo } from 'react';
+import { Group } from '@/types/types';
+
+interface GroupListProps {
+  groups: Group[];
+}
+
+const getDefaultGroup = (groups: Group[]) => {
+  return groups.find((group: Group) => group.groupType === 'default') || null;
+};
+
+const filterDefaultGroup = (groups: Group[]) => {
+  return groups.filter((group) => group.groupType !== 'default');
+};
+
+const GroupList: React.FC<GroupListProps> = ({ groups }) => {
+  const defaultGroup = useAppStore((state) => state.defaultGroup);
+  const setDefaultGroup = useAppStore((state) => state.setDefaultGroup);
+
+  useEffect(() => {
+    const defaultGroupData = getDefaultGroup(groups);
+    if (defaultGroupData) {
+      setDefaultGroup(defaultGroupData);
+    }
+  }, [groups, setDefaultGroup]);
+
+  const sortedGroups = useMemo(
+    () =>
+      filterDefaultGroup(groups).sort(
+        (a, b) =>
+          new Date(b.createdDate).getTime() - new Date(a.createdDate).getTime(),
+      ),
+    [groups],
+  );
+
+  return (
+    <div className='grid grid-cols-1 gap-2 p-2'>
+      {defaultGroup && (
+        <GroupCard
+          key={defaultGroup.groupId}
+          group={defaultGroup}
+        />
+      )}
+      {sortedGroups.map((group) => (
+        <GroupCard
+          key={group.groupId}
+          group={group}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default GroupList;
