ad142fd8f5e45408ee26dd56440c817209eca984
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-02-06 10:52:21 +0530

    feat: add useGroupMembers hook to manage group member data retrieval

---
+++ src/hooks/useGroupMembers.ts
@@ -0,0 +1,36 @@
+import { useEffect, useMemo, useState } from 'react';
+import useAppStore from '@/stores/useAppStore';
+import { Group, User } from '@/types/types';
+
+const useGroupMembers = (groupId: string | string[] | undefined) => {
+  const resolvedGroupId = useMemo(
+    () => (Array.isArray(groupId) ? groupId[0] : groupId),
+    [groupId],
+  );
+  const getGroups = useAppStore((state) => state.groups);
+  const getUsers = useAppStore((state) => state.users);
+
+  const [members, setMembers] = useState<User[]>([]);
+
+  useEffect(() => {
+    if (resolvedGroupId) {
+      // Retrieve the group information using the groupId from the Zustand store
+      const groupData = getGroups.find(
+        (group: Group) => group.groupId === resolvedGroupId,
+      );
+      if (groupData) {
+        // Retrieve the users associated with the memberIds
+        const memberData = groupData.memberIds
+          .map((memberId) =>
+            getUsers.find((user: User) => user.userId === memberId),
+          )
+          .filter((user): user is User => user !== undefined);
+        setMembers(memberData);
+      }
+    }
+  }, [resolvedGroupId, getGroups, getUsers]);
+
+  return members;
+};
+
+export default useGroupMembers;
