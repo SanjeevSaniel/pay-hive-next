eb59f78570366e5e1f2724d5376a60b5fa65ed3a
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-06-16 18:15:52 +0530

    refactor: adjust Header component styling and button icon size for improved UI consistency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,20 +14,20 @@ const Header = () => {
   }
 
   return (
-    <div className='flex justify-between items-center px-4 py-3 bg-inherit text-[#d1d3d7] rounded-xl sticky top-0 z-20'>
+    <div className='flex justify-between items-center px-4 py-2 bg-inherit text-[#d1d3d7] rounded-xl sticky top-0 z-20'>
       <Link
         href={basePath}
         className='flex items-center gap-2 text-[#f9ffff] font-extrabold'>
         <span className={`${neodaFont.className} text-xl mb-1.5`}>S</span>
-        <span className='text-xl text-[#d1d3d7]'>Splitly</span>
+        <span className='text-lg text-[#d1d3d7]'>Splitly</span>
       </Link>
       {/* <Link
         href={basePath}
         className='flex items-center text-lg font-extrabold'></Link> */}
       <Button
         isIconOnly
-        className='p-1 rounded-full'>
-        <Bell size={20} />
+        className='p-0.5 rounded-full'>
+        <Bell size={16} />
       </Button>
     </div>
   );
