5c79843d964fbe769924623fa135c8e087d2e145
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-06-23 18:11:55 +0530

    refactor: restructure GroupsPage layout for improved organization and styling

--- src/app/v1/[userId]/groups/page.tsx
+++ src/app/v1/[userId]/groups/page.tsx
@@ -28,13 +28,16 @@ const GroupsPage = () => {
   ]);
 
   return (
-    <div className='flex flex-col gap-2 py-2 w-full relative'>
-      {/* <Header /> */}
-      <TotalSpend />
-      <GroupHeaderSection groupsCount={groups.length} />
-      {/* <Suspense fallback={<Loading />}> */}
-      <GroupList groups={groups} />
-      {/* </Suspense> */}
+    <div className='grid grid-cols-3 gap-2 p-2 h-full'>
+      <div className='col-span-2 flex flex-col gap-2 py-2 w-full overflow-auto'>
+        <GroupHeaderSection groupsCount={groups.length} />
+        {/* <Suspense fallback={<Loading />}> */}
+        <GroupList groups={groups} />
+        {/* </Suspense> */}
+      </div>
+      <div className='overflow-hidden'>
+        <TotalSpend />
+      </div>
     </div>
   );
 };
